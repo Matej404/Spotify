737279e79167b0bcc136ecabddeb402200d64b09
Matej404 <matejjurin278@gmail.com>
2022-04-04 20:28:15 +0100

    header-responsive

--- src/web/navbar/Navbar.js
+++ src/web/navbar/Navbar.js
@@ -1,51 +1,82 @@
 import "./navbar.css";
 import { NavLink } from "react-router-dom";
 import { BsSpotify } from "react-icons/bs";
-import { Fade } from "react-reveal";
+import { FaBars } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const handleToggleMenu = () => {
+        setToggleMenu(!toggleMenu);
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth);
+
+        return () => {
+            window.removeEventListener('resize', changeWidth);
+        }
+    }, []);
 
     return(
         <div className="navbar">
             <NavLink to="/spotify" className="navlink">
-                <div className="navbar-spotify">
-                    <BsSpotify className="icon" />
-                    <p>Spotify</p>
+                    <div className="navbar-spotify">
+                        <BsSpotify className="icon" />
+                        <p>Spotify</p>
+                    </div>
+                </NavLink>
+                {(toggleMenu || screenWidth > 900) && (
+            <div className="navbar-container">
+                <div className="close-btn" onClick={handleToggleMenu}>
+                    <IoCloseSharp />
                 </div>
-            </NavLink>
-            <Fade right>
-            <div className="navbar-right-side">
-                <ul className="nav-menu">
-                    <li className="nav-link">
-                        <NavLink to="/premium" className="navlink-menu">
-                            Premium
-                        </NavLink>
-                    </li>
-                    <li className="nav-link">
-                        <NavLink to="/support" className="navlink-menu">
-                            Support
-                        </NavLink>
-                    </li>
-                    <li className="nav-link">
-                        <NavLink to="/download" className="navlink-menu">
-                            Download
-                        </NavLink>
-                    </li> 
-                </ul>
-                <hr />
-                <ul className="link-menu">
-                    <li>
-                        <a href="https://www.spotify.com/hr-en/signup">Sign up</a>
-                    </li>
-                    <li>
-                        <a href="https://accounts.spotify.com/en/login/?">Log in</a>
-                    </li>
-                </ul>
-                <div>
-
+                <div className="navbar-right-side">
+                    <ul className="nav-menu">
+                        <li className="nav-link">
+                            <NavLink to="/premium" className="navlink-menu">
+                                Premium
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink to="/support" className="navlink-menu">
+                                Support
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink to="/download" className="navlink-menu">
+                                Download
+                            </NavLink>
+                        </li> 
+                    </ul>
+                    <div className="slash" />
+                    <ul className="link-menu">
+                        <li>
+                            <a href="https://www.spotify.com/hr-en/signup">Sign up</a>
+                        </li>
+                        <li>
+                            <a href="https://accounts.spotify.com/en/login/?">Log in</a>
+                        </li>
+                    </ul>
+                    <div>
+                    </div>
                 </div>
+                <NavLink to="/spotify" className="navlink navlink2" >
+                    <div className="navbar-spotify">
+                        <BsSpotify className="icon" />
+                        <p>Spotify</p>
+                    </div>
+                </NavLink>
+            </div>)}
+            <div className="bars-icon" onClick={handleToggleMenu}>
+                <FaBars />
             </div>
-            </Fade>
         </div>
     )
 }
